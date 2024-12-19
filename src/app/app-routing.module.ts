81d71d4c2824b5fefc79a222670d77220a9ea72f
Stephano0207 <castanedastephano16@gmail.com>
2024-12-19 07:47:53 -0500

    Plantilla cero creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,26 @@ const routes: Routes = [
     path: 'validation',
     loadChildren: () => import('./login/validation/validation.module').then( m => m.ValidationPageModule)
   },
+  {
+    path: 'create',
+    loadChildren: () => import('./empleado/create/create.module').then( m => m.CreatePageModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./empleado/edit/edit.module').then( m => m.EditPageModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./departamento/edit/edit.module').then( m => m.EditPageModule)
+  },
+  {
+    path: 'create',
+    loadChildren: () => import('./departamento/create/create.module').then( m => m.CreatePageModule)
+  },
+  {
+    path: 'grafico',
+    loadChildren: () => import('./departamento/grafico/grafico.module').then( m => m.GraficoPageModule)
+  },
 ];
 
 @NgModule({
